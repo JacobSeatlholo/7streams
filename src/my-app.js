524475bd22a2077b9f0dcced10a597026be70f8b
Jacob Seatlholo <66915934+JacobSeatlholo@users.noreply.github.com>
2022-06-14 08:18:45 +0200

    Update my-app.js

--- src/my-app.js
+++ src/my-app.js
@@ -111,7 +111,7 @@ class MyApp extends PolymerElement {
                 icon="my-icons:menu"
                 drawer-toggle=""
               ></paper-icon-button>
-              <div main-title="">My App</div>
+              <div main-title="">7Streams</div>
             </app-toolbar>
           </app-header>
 
